afcbe693790523ed56f229e3578e9a227a645163
Christophe Tiraoui <krux@krux.fr>
2023-01-31 08:20:56 +0100

    controlled exec ?

--- src/helpers/exec.ts
+++ src/helpers/exec.ts
@@ -3,9 +3,19 @@ import childProc from "child_process";
 export async function exec(cmd: string, cwd: string): Promise<number> {
   console.debug("exec async", cmd, "inside", cwd);
   return await new Promise<number>((resolve, reject) => {
-    const ps = childProc.exec(String(cmd), {
+    let executable = cmd;
+    let args: string[] = [];
+    const cmdSegments = cmd.split(" ");
+    if (cmdSegments.length > 0) {
+      executable = cmdSegments[0];
+      cmdSegments.splice(0, 1);
+      args = cmdSegments;
+    }
+    console.info(">", executable, args);
+    const ps = childProc.execFile(executable, args, {
       encoding: "utf-8",
       cwd,
+      shell: true,
     });
     if (ps.stdin != null) ps.stdin.pipe(process.stdin);
     if (ps.stdout != null) ps.stdout.pipe(process.stdout);
