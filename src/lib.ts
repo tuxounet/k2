1cf095d3aea1f28e968b012753ed2040e8e820da
Christophe Tiraoui <tuxounet@gmail.com>
2023-03-15 21:33:40 +0100

    more exports

--- src/lib.ts
+++ src/lib.ts
@@ -4,3 +4,6 @@ export * as inventory from "./inventory/inventory";
 export * as kinds from "./inventory/kinds";
 export * as template from "./inventory/template";
 export * as scripts from "./inventory/scripts";
+export * as namespace from "./helpers/namespace";
+export * as hash from "./helpers/hash";
+export * as exec from "./helpers/exec";
