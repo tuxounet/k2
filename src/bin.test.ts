1d830de7ca47670f1e019643c6c0b1d599ceb24e
Christophe Tiraoui <tuxounet@gmail.com>
2023-08-13 23:15:49 +0200

    try to add test

--- src/bin.test.ts
+++ src/bin.test.ts
@@ -1,5 +1,16 @@
+import { run } from "./bin";
+
 describe("index", () => {
-  test("first test", async () => {
-    expect(true).toBeTruthy();
+  test("showing help", async () => {
+    const processExit = jest
+      .spyOn(process, "exit")
+      .mockImplementation((code?: number) => undefined as never);
+    run([
+      "/usr/local/bin/node",
+      "/Users/krux/repos/github.com/k2/dist/bin.js",
+      "help",
+    ]);
+    expect(processExit).toHaveBeenCalledWith(1);
+    processExit.mockRestore();
   });
 });
